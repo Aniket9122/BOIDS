bdf28b7de00a9dd0d6930577b1caf361cf628e88
Victor Gachoki Mwendia <el20vgm@leeds.ac.uk>
2025-04-17 15:53:28 +0100

    Base Environment Creation and Velocity Changes

---
+++ boids.py
@@ -0,0 +1,19 @@
+import pygame
+from Environment.base_env import BaseEnvironment
+
+pygame.init()
+
+env = BaseEnvironment(1000, 800)
+env.populate_environment()
+env.render()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    env.update()
+    env.render()
+
+pygame.quit()
